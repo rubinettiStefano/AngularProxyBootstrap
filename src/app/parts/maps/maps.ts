40ae7004e8c24aadd1d289e0219320234251d065
pier <pierfrancesco.milani@gmail.com>
2025-10-01 19:28:15 +0200

    consegna 1 (navbar e homepage)

---
+++ src/app/parts/maps/maps.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maps',
+  imports: [],
+  templateUrl: './maps.html',
+  styleUrl: './maps.css'
+})
+export class Maps {
+
+}
