f49d2b913c1d60afe23cab87f523e3891fe579c3
StefanoR <rubinetti.stefano@gmail.com>
2025-10-01 17:33:20 +0200

    consegna 1 (navbar e homepage)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {Homepage} from './pages/homepage/homepage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",component:Homepage}
+
+];
