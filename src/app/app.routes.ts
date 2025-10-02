2cc7442d398dbb4f646b0d209f49f86ecfe942c1
StefanoR <rubinetti.stefano@gmail.com>
2025-10-02 11:58:31 +0200

    consegna 1 (navbar e homepage)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {Homepage} from './pages/homepage/homepage';
 import {AllBooks} from './pages/all-books/all-books';
+import {AllAuthors} from './pages/all-authors/all-authors';
 
 export const routes: Routes = [
   {path:"",component:Homepage},
   {path:"books",component:AllBooks},
+  {path:"authors",component:AllAuthors},
 
 ];
---
+++ src/model/GetAuthorDto.ts
@@ -0,0 +1,4 @@
+export interface GetAuthorDto
+{
+  //TODO 1 - Mettere gli stessi campi dell'output dto del backend
+}
